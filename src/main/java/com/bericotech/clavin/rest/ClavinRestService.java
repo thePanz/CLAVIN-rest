6a4b467a82c2fd7a3d4baa6636bdbe4bde740c11
JGlesner <jeremy.glesner@gmail.com>
2015-02-12 15:02:37 -0500

    Debug level logging added to CLAVIN-REST.

--- src/main/java/com/bericotech/clavin/rest/ClavinRestService.java
+++ src/main/java/com/bericotech/clavin/rest/ClavinRestService.java
@@ -17,37 +17,78 @@ import com.bericotech.clavin.GeoParser;
 import com.bericotech.clavin.nerd.StanfordExtractor;
 import com.bericotech.clavin.rest.resource.ClavinRestResource;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 
-
-public class ClavinRestService extends Service<ClavinRestConfiguration> {
+public class ClavinRestService extends Service<ClavinRestConfiguration> { 
 	
     public static void main(String[] args) throws Exception {
+		Logger logger = LoggerFactory.getLogger(ClavinRestService.class);
+        
+		logger.debug("Start of the 'main' method.");
+		logger.info("CLAVIN-REST is starting...");
+    	
+        logger.debug("About to execute the RUN method.");
         new ClavinRestService().run(args);
+        logger.debug("Returned from executing the RUN method");
+        
+        logger.debug("End of the 'main' method.");
     }
 
     @Override
     public void initialize(Bootstrap<ClavinRestConfiguration> bootstrap) {
-        bootstrap.setName("clavin-rest");
-        //bootstrap.addBundle(new AssetsBundle("/assets/", "/"));
-        bootstrap.addBundle(new ConfiguredAssetsBundle("/assets/", "/"));
-        bootstrap.addCommand(new IndexCommand());
+    	Logger logger = LoggerFactory.getLogger(ClavinRestService.class);
+    	logger.debug("Start of the 'initialize' method.");
+    	
+    	logger.debug("About to use dropwizard bootstrap to setName 'clavin-rest'.");
+    	bootstrap.setName("clavin-rest");
+    	logger.debug("Finished setName.");
+    	
+    	//bootstrap.addBundle(new AssetsBundle("/assets/", "/"));
+
+    	logger.debug("About to use dropwizard bootstrap to addBundle 'assets'.");
+    	bootstrap.addBundle(new ConfiguredAssetsBundle("/assets/", "/"));
+    	logger.debug("Finished addBundle.");
+    	
+    	logger.debug("About to use dropwizard bootstrap to addCommand IndexCommand.");
+    	bootstrap.addCommand(new IndexCommand());
+    	logger.debug("Finished addCommand.");
+    	
+    	logger.debug("End of the 'initialize' method.");
     }
 
     @Override
     public void run(ClavinRestConfiguration configuration,
                     Environment environment) throws ClassCastException, ClassNotFoundException, IOException, ParseException, ClavinException {
-        final String luceneDir = configuration.getLuceneDir();
+    	
+    	Logger logger = LoggerFactory.getLogger(ClavinRestService.class);
+    	
+    	logger.debug("Start of the 'run' method.");
+    	
+    	logger.debug("Setting environment variables for luceneDir, maxHitDepth, maxContextWindow.");
+    	final String luceneDir = configuration.getLuceneDir();
         final Integer maxHitDepth = configuration.getMaxHitDepth();
         final Integer maxContextWindow = configuration.getMaxContextWindow();
         // final Boolean fuzzy = configuration.getFuzzy();
-             
-        Gazetteer gazetteer = new LuceneGazetteer(new File(luceneDir));
-
-        StanfordExtractor extractor = new StanfordExtractor();  	   	
-        GeoParser parser = new GeoParser(extractor, gazetteer, maxHitDepth, maxContextWindow, false);
         
+        logger.debug("Initializing the Lucene Gazetteer.");
+        Gazetteer gazetteer = new LuceneGazetteer(new File(luceneDir));
+        logger.debug("Finished initializing gazetteer.");
+        
+        logger.debug("Initializing the CLAVIN-NERD 'StanfordExtractor'.");
+        StanfordExtractor extractor = new StanfordExtractor();
+        logger.debug("Finished initializing StanfordExtractor.");
+        
+        logger.debug("Initializing the CLAVIN 'GeoParser'.");
+        GeoParser parser = new GeoParser(extractor, gazetteer, maxHitDepth, maxContextWindow, false);
+        logger.debug("Finished initializing GeoParser.");
+        
+        logger.debug("Registering the 'ClavinRestResource(parser)' with DropWizard.");
         environment.addResource(new ClavinRestResource(parser));
+        logger.debug("Finished registering the Resource environment.");
+        
+        logger.debug("End of the 'run' method.");
     }
 
 }
--- src/main/java/com/bericotech/clavin/rest/resource/ClavinRestResource.java
+++ src/main/java/com/bericotech/clavin/rest/resource/ClavinRestResource.java
@@ -7,9 +7,15 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.List;
+
 import com.bericotech.clavin.GeoParser;
 import com.bericotech.clavin.resolver.ResolvedLocation;
+import com.bericotech.clavin.rest.ClavinRestService;
 import com.bericotech.clavin.rest.core.ResolvedLocations;
 import com.bericotech.clavin.rest.core.ResolvedLocationsMinimum;
 
@@ -18,6 +24,8 @@ import com.bericotech.clavin.rest.core.ResolvedLocationsMinimum;
 public class ClavinRestResource {
     private final GeoParser parser;    
 
+    final Logger logger = LoggerFactory.getLogger(ClavinRestService.class);
+    
     public ClavinRestResource(GeoParser parser) {
         this.parser = parser;
     }
@@ -33,17 +41,34 @@ public class ClavinRestResource {
     @Consumes(MediaType.TEXT_PLAIN)
     @Produces(MediaType.APPLICATION_JSON)
     public Response extractAndResolveSimpleLocationsFromText(String text) {
-    
+    	
+    	logger.debug("Start of POST Method for path '/geotag'.");
+    	
+    	
         ResolvedLocations result = null;
+        
+        logger.debug("Entering Try/Catch Block for parsing 'text' @POST '/geotag'.");
         try {
+        	logger.debug("In Try Block, about to parse 'text' @POST '/geotag', returning 'List<ResolvedLocation>'.");
             List<ResolvedLocation> resolvedLocations = parser.parse(text);
-        result = new ResolvedLocations(resolvedLocations);
+            logger.debug("In Try Block, finished parsing 'text' @POST '/geotag'.");
+            
+            logger.debug("In Try Block, about to set result = 'ResolvedLocations' @POST '/geotag'.");
+            result = new ResolvedLocations(resolvedLocations);
+            logger.debug("In Try Block, finished setting result = 'ResolvedLocations' @POST '/geotag'.");
         
         } catch (Exception e) {
-            e.printStackTrace();
+        	
+        	logger.debug("In Catch Block, an error has occured @POST '/geotag'.");
+        	logger.error("Error Message In Catch Block @POST '/geotag': " + e.getMessage()); 
+        	e.printStackTrace();
+            
+            logger.debug("In Catch Block, about to return @POST '/geotag'.");
             return Response.status(500).entity(e).build();
         }
+        logger.debug("Exiting Try/Catch Block for parsing 'text' @POST '/geotag'.");
       
+        logger.debug("End of POST Method for path '/geotag'; Returning...");
         return Response.status(200).entity(result).build();
         
     }
@@ -55,21 +80,35 @@ public class ClavinRestResource {
     @Produces(MediaType.APPLICATION_JSON)
     public Response extractAndResolveSimpleShortLocationsFromText(String text) {
     
+    	logger.debug("Start of POST Method for path '/geotagmin'.");
+    	
         ResolvedLocationsMinimum result = null;
+        
+        logger.debug("Entering Try/Catch Block for parsing 'text' @POST '/geotagmin'.");
         try {
-            List<ResolvedLocation> resolvedLocations = parser.parse(text);
-            result = new ResolvedLocationsMinimum(resolvedLocations);
             
+        	logger.debug("In Try Block, about to parse 'text' @POST '/geotagmin', returning 'List<ResolvedLocation>'.");
+        	List<ResolvedLocation> resolvedLocations = parser.parse(text);
+        	logger.debug("In Try Block, finished parsing 'text' @POST '/geotagmin'.");
+        	
+        	logger.debug("In Try Block, about to set result = 'ResolvedLocations' @POST '/geotagmin'.");
+            result = new ResolvedLocationsMinimum(resolvedLocations);
+            logger.debug("In Try Block, finished setting result = 'ResolvedLocations' @POST '/geotagmin'.");
         
         } catch (Exception e) {
+        	
+        	logger.debug("In Catch Block, an error has occured @POST '/geotagmin'.");
+        	logger.error("Error Message In Catch Block @POST '/geotagmin': " + e.getMessage()); 
             e.printStackTrace();
+            
+            logger.debug("In Catch Block, about to return @POST '/geotagmin'.");
             return Response.status(500).entity(e).build();
         }
+        logger.debug("Exiting Try/Catch Block for parsing 'text' @POST '/geotagmin'.");
       
+        logger.debug("End of POST Method for path '/geotagmin'; Returning...");
         return Response.status(200).entity(result).build();
         
     }
-    
-    
-    
+  
 }
