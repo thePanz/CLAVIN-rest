b28806a55282bb7666f0dffce8c219a2477e4e10
Emanuele Panzeri <thepanz@gmail.com>
2015-04-02 16:56:59 +0200

    Added FeatureCode and FeatureClass to ResolvedLocationMinimum

--- src/main/java/com/bericotech/clavin/rest/core/ResolvedLocationMinimum.java
+++ src/main/java/com/bericotech/clavin/rest/core/ResolvedLocationMinimum.java
@@ -9,13 +9,17 @@ public class ResolvedLocationMinimum {
 	public final String countryCode;
 	public final double latitude;
 	public final double longitude;
-	
+	public final String featureClass;
+	public final String featureCode;
+
 	ResolvedLocationMinimum(ResolvedLocation rl) {
 		this.geonameID = rl.getGeoname().getGeonameID();
 		this.name = rl.getGeoname().getName();
 		this.countryCode = rl.getGeoname().getPrimaryCountryCode().toString();
 		this.latitude = rl.getGeoname().getLatitude();
 		this.longitude= rl.getGeoname().getLongitude();
+		this.featureClass = rl.getGeoname().getFeatureClass().type;
+		this.featureCode = rl.getGeoname().getFeatureCode().getType();
 	}
 
 }
